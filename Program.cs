e8d32b6e8932edb2db8c4e333221122b1c6eb1b0
Brian P. Hamachek <brian@brianhama.com>
2023-04-28 13:10:28 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿using MathNet.Numerics.IntegralTransforms;
+using NAudio.Wave;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace PoliceRadioMonitor
+{
+    class Program
+    {
+        private const string API_KEY = "";
+        private const string SEARCH_KEYWORD = "";
+
+        private const int SampleRate = 44100;
+        private const double Threshold = 400000;
+
+        private static bool isRecording = false;
+        private static List<short> recordedData = new List<short>();
+
+        static void Main(string[] args)
+        {
+            using (var audioInput = new WaveInEvent())
+            {
+                audioInput.DeviceNumber = 0;
+                audioInput.WaveFormat = new WaveFormat(SampleRate, 1);
+                audioInput.BufferMilliseconds = 1000;
+                audioInput.DataAvailable += AudioInput_DataAvailable;
+
+                audioInput.StartRecording();
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+
+                audioInput.StopRecording();
+            }
+        }
+
+        private static void AudioInput_DataAvailable(object? sender, WaveInEventArgs e)
+        {
+            short[] audioData = new short[e.BytesRecorded / 2];
+            Buffer.BlockCopy(e.Buffer, 0, audioData, 0, e.BytesRecorded);
+
+            var complexData = new MathNet.Numerics.Complex32[audioData.Length];
+            for (int i = 0; i < audioData.Length; i++)
+            {
+                complexData[i] = new MathNet.Numerics.Complex32(audioData[i], 0.0f);
+            }
+
+            Fourier.Forward(complexData, FourierOptions.NoScaling);
+
+            float maxAmplitude = 0;
+            for (int i = 0; i < complexData.Length / 2; i++)
+            {
+                float amplitude = complexData[i].Magnitude;
+                if (amplitude > maxAmplitude)
+                {
+                    maxAmplitude = amplitude;
+                }
+            }
+
+            if (maxAmplitude > Threshold)
+            {
+                isRecording = true;
+                recordedData.AddRange(audioData);
+            }
+            else if (isRecording)
+            {
+                SaveTransmission();
+                isRecording = false;
+                recordedData.Clear();
+            }
+        }
+
+        private static void SaveTransmission()
+        {
+            using (MemoryStream waveStream = new MemoryStream())
+            {
+                using (var waveFileWriter = new WaveFileWriter(waveStream, new WaveFormat(SampleRate, 1)))
+                {
+                    waveFileWriter.WriteSamples(recordedData.ToArray(), 0, recordedData.Count);
+                }
+
+                string transcription = WhisperTranscribe(waveStream.GetBuffer());
+                Console.WriteLine("Transcription: " + transcription);
+                if (transcription.ToLower().Contains(SEARCH_KEYWORD.ToLower())) {
+                    KeywordDetected();
+                }
+            }
+        }
+
+        private static void KeywordDetected()
+        {
+            Console.WriteLine("DETECTED");
+        }
+
+        static string WhisperTranscribe(byte[] fileBytes)
+        {
+            RestClient client = new RestClient("https://api.openai.com");
+            RestRequest request = new RestRequest("/v1/audio/transcriptions", Method.Post);
+            request.AddHeader("Authorization", $"Bearer {API_KEY}");
+            request.AddFile("file", fileBytes, "audio.wav");
+            request.AddParameter("model", "whisper-1");
+
+            RestResponse response = client.Execute(request);
+
+            if (response.IsSuccessful && response.Content != null)
+            {
+                TranscriptionResponse transcriptionResponse = JsonConvert.DeserializeObject<TranscriptionResponse>(response.Content);
+                if (transcriptionResponse != null && String.IsNullOrEmpty(transcriptionResponse.text) == false)
+                {
+                    return transcriptionResponse.text;
+                }
+            }
+
+            return "";
+        }
+
+        class TranscriptionResponse
+        {
+            public String? text { get; set; }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # police-radio-monitor
  This simple script uses OpenAI's Whsiper model to monitor a police radio channel for a given keyword.
+
+## Background
+I reside in Palo Alto, where I'm surrounded by elderly neighbors who enjoy contacting the police whenever they think my music is too loud. Given the nature of Palo Alto, the police have little else to handle, so they take these complaints very seriously.
+
+To outsmart my neighbors and showcase the capabilities and simplicity of OpenAI's new speech transcription model, Whisper, I devised a straightforward script. For several years, I've been streaming the Palo Alto Police's dispatch radio transmissions on www.paloaltopolice.org. To accomplish this, I installed an antenna on my garage roof and employed a Uniden TrunkTracker to decode the Silicon Valley Regional Communications System's P25 Phase II radio system. Although I attempted to use SDR instead of Uniden, the latter proved to provide clearer decoding. The Uniden device connects to a basement computer, and the radio transmissions are forwarded to a service for archiving.
+
+I noticed that every time the police address a noise complaint at my residence, a radio transmission containing my home address is dispatched so that an officer can respond. This C# script in the repository listens to the same audio stream. It establishes an audio stream using NAudio, processes the audio data with the MathNet.Numerics library, and detects radio transmissions based on a simple threshold. When the maximum amplitude of the audio signal's frequency spectrum surpasses the threshold, an in-memory audio file recording commences. The recording ceases when the amplitude returns to baseline, and the audio segment is then sent to OpenAI's Whisper API for transcoding.
+
+After transcoding, the text is searched for a specific keyword - in this case, my home's street name. Since my street is short, this approach shouldn't produce many false positives. If the keyword is detected, an HTTP request is sent to my home automation controller (Control4), which subsequently turns off the entire-house audio system and displays a text alert on the wall screens.
+
+I should clarify that I don't intend to use this script to actually undermine the police or annoy my neighbors. However, it serves as an excellent example of the power and simplicity of OpenAI's new Whisper model.
